95de05b9a1c9491cc826c9906d00af200e4b2ba9
aikawaYO <makonoku@yahoo.co.jp>
2021-02-18 22:29:39 +0900

    add USB sirial

--- src/hal/hal.c
+++ src/hal/hal.c
@@ -8,6 +8,15 @@ bool alarm_irq(struct repeating_timer *t) {
 
 void hal_init(void){
     add_repeating_timer_ms(1, alarm_irq, NULL, &timer);
-    clocks_hw->sleep_en0 = 0x0;
-    clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS;
+    clocks_hw->sleep_en0 = 0;
+    clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS | CLOCKS_SLEEP_EN1_CLK_SYS_USBCTRL_BITS | CLOCKS_SLEEP_EN1_CLK_USB_USBCTRL_BITS;
+}
+
+int hal_write(int fd, const void *buf, int nbytes){
+    printf(buf);
+    return nbytes;
+}
+
+int hal_flush(int fd) {
+    return 0;
 }
